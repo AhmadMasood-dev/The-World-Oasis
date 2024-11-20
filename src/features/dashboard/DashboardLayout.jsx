935a0f614146f5afb50732a2c98494a139a82c01
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-11-20 14:05:45 +0500

    get the afterdate booking and stays form the database

---
+++ src/features/dashboard/DashboardLayout.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
+
+const StyledDashboardLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto 34rem auto;
+  gap: 2.4rem;
+`;
+
+function DashboardLayout() {
+  const { isLoading, bookings } = useRecentBookings();
+  const { isLoading: isLoading2, stays, confirmStays } = useRecentStays();
+  if (isLoading || isLoading2) return <Spinner />;
+  console.log(bookings);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Chart stay duration</div>
+      <div>Chart Sale</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
